a59f42d638def1d145473bff8aad3fa82224fe63
polyfloyd <floyd@polyfloyd.net>
2017-05-18 03:15:22 +0200

    tag: Translate the genre ID when converting from ID3v1

--- src/tag.rs
+++ src/tag.rs
@@ -282,8 +282,34 @@ impl<'a> Tag {
         if tag_v1.genre_str.is_some() {
             tag.set_genre(tag_v1.genre_str.unwrap());
         } else if tag_v1.genre.is_some() {
-            // TODO maybe convert this from the genre id into a string
-            tag.set_genre(format!("{}", tag_v1.genre.unwrap()));
+            let genre_list = [
+                "Blues", "Classic Rock", "Country", "Dance", "Disco", "Funk", "Grunge", "Hip-Hop",
+                "Jazz", "Metal", "New Age", "Oldies", "Other", "Pop", "R&B", "Rap", "Reggae",
+                "Rock", "Techno", "Industrial", "Alternative", "Ska", "Death Metal", "Pranks",
+                "Soundtrack", "Euro-Techno", "Ambient", "Trip-Hop", "Vocal", "Jazz+Funk", "Fusion",
+                "Trance", "Classical", "Instrumental", "Acid", "House", "Game", "Sound Clip",
+                "Gospel", "Noise", "Alternative Rock", "Bass", "Soul", "Punk", "Space",
+                "Meditative", "Instrumental Pop", "Instrumental Rock", "Ethnic", "Gothic",
+                "Darkwave", "Techno-Industrial", "Electronic", "Pop-Folk", "Eurodance", "Dream",
+                "Southern Rock", "Comedy", "Cult", "Gangsta", "Top 40", "Christian Rap",
+                "Pop/Funk", "Jungle", "Native US", "Cabaret", "New Wave", "Psychadelic", "Rave",
+                "Showtunes", "Trailer", "Lo-Fi", "Tribal", "Acid Punk", "Acid Jazz", "Polka",
+                "Retro", "Musical", "Rock & Roll", "Hard Rock", "Folk", "Folk-Rock",
+                "National Folk", "Swing", "Fast Fusion", "Bebob", "Latin", "Revival", "Celtic",
+                "Bluegrass", "Avantgarde", "Gothic Rock", "Progressive Rock", "Psychedelic Rock",
+                "Symphonic Rock", "Slow Rock", "Big Band", "Chorus", "Easy Listening", "Acoustic",
+                "Humour", "Speech", "Chanson", "Opera", "Chamber Music", "Sonata", "Symphony",
+                "Booty Bass", "Primus", "Porn Groove", "Satire", "Slow Jam", "Club", "Tango",
+                "Samba", "Folklore", "Ballad", "Power Ballad", "Rhytmic Soul", "Freestyle", "Duet",
+                "Punk Rock", "Drum Solo", "Acapella", "Euro-House", "Dance Hall", "Goa",
+                "Drum & Bass", "Club-House", "Hardcore", "Terror", "Indie", "BritPop", "Negerpunk",
+                "Polsk Punk", "Beat", "Christian Gangsta", "Heavy Metal", "Black Metal",
+                "Crossover", "Contemporary C", "Christian Rock", "Merengue", "Salsa",
+                "Thrash Metal", "Anime", "JPop", "SynthPop",
+            ];
+            if let Some(genre) = genre_list.get(tag_v1.genre.unwrap() as usize) {
+                tag.set_genre(genre.to_string());
+            }
         }
 
         Ok(tag)
