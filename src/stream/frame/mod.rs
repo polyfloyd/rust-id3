0ef80bcf5258a9545711182ac4c9d116f6489886
polyfloyd <floyd@polyfloyd.net>
2017-05-28 02:30:43 +0200

    strea/frame: Remove unused macro

--- src/stream/frame/mod.rs
+++ src/stream/frame/mod.rs
@@ -6,26 +6,10 @@ use ::tag;
 use ::frame::Frame;
 use ::stream::encoding::Encoding;
 
-
-macro_rules! id_or_padding {
-    ($reader:ident, $n:expr) => {
-        {
-            let mut buf = [0u8; $n];
-            try!($reader.read(&mut buf[..1]));
-            if buf[0] == 0 { // padding
-                return Ok(None);
-            }
-            try!($reader.read(&mut buf[1..]));
-            try!(String::from_utf8(buf.to_vec()))
-        }
-
-    };
-}
-
-pub mod v2;
-pub mod v3;
-pub mod v4;
-pub mod content;
+mod v2;
+mod v3;
+mod v4;
+mod content;
 
 pub fn decode<R>(reader: &mut R, version: tag::Version, unsynchronization: bool) -> ::Result<Option<(usize, Frame)>>
     where R: io::Read {
