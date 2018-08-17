c9d3e704fb73680aa48600bcd06a7ed5a577c6da
polyfloyd <floyd@polyfloyd.net>
2018-08-17 15:47:51 +0200

    Fix unsynch decoder producing corrupted output in some cases

--- src/stream/unsynch.rs
+++ src/stream/unsynch.rs
@@ -2,7 +2,6 @@
 //! existing software and hardware. There is no use in 'unsynchronising' tags if the file is only
 //! to be processed only by ID3v2 aware software and hardware. Unsynchronisation is only useful
 //! with tags in MPEG 1/2 layer I, II and III, MPEG 2.5 and AAC files.
-use std::cmp;
 use std::io;
 use std::mem;
 
@@ -51,33 +50,35 @@ impl<R> Reader<R>
 impl<R> io::Read for Reader<R>
     where R: io::Read {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        if self.next >= self.available {
-            self.available = self.reader.read(&mut self.buf)?;
-            self.next = 0;
-            if self.available == 0 {
-                return Ok(0);
+        let mut i = 0;
+
+        while i < buf.len() {
+            assert!(self.next <= self.available);
+            if self.next == self.available {
+                self.available = self.reader.read(&mut self.buf)?;
+                self.next = 0;
+                if self.available == 0 {
+                    break;
+                }
             }
-        }
-        if self.discard_next_null_byte && self.buf[self.next] == 0x00 {
+
+            if self.discard_next_null_byte && self.buf[self.next] == 0x00 {
+                self.discard_next_null_byte = false;
+                self.next += 1;
+                continue;
+            }
+            self.discard_next_null_byte = false;
+
+            buf[i] = self.buf[self.next];
+            i += 1;
+
+            if self.buf[self.next] == 0xff {
+                self.discard_next_null_byte = true;
+            }
             self.next += 1;
         }
-        self.discard_next_null_byte = false;
 
-        let max = cmp::min(self.available - self.next, buf.len());
-        let mut copy_range = self.next..self.next;
-        while copy_range.end - copy_range.start < max {
-            copy_range.end += 1;
-            if self.buf[copy_range.end - 1] == 0xff {
-                self.discard_next_null_byte = true;
-                break;
-            }
-        }
-
-        let copy_num = copy_range.end - copy_range.start;
-        buf[0..copy_num].copy_from_slice(&self.buf[copy_range]);
-        self.next += copy_num;
-        assert!(self.next <= self.available);
-        Ok(copy_num)
+        Ok(i)
     }
 }
 
@@ -109,17 +110,40 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_synchsafe() {
+    fn synchsafe() {
         assert_eq!(681_570, encode_u32(176_994));
         assert_eq!(176_994, decode_u32(681_570));
     }
 
     #[test]
-    fn test_synchronization() {
+    fn synchronization() {
         let mut v = vec![66, 0, 255, 0, 255, 0, 0, 255, 66];
         encode_vec(&mut v);
         assert_eq!(v, [66, 0, 255, 0, 0, 255, 0, 0, 0, 255, 66]);
         decode_vec(&mut v);
         assert_eq!(v, [66, 0, 255, 0, 255, 0, 0, 255, 66]);
+    }
+
+    #[test]
+    fn synchronization_jpeg() {
+        let orig = vec![0xff, 0xd8, 0xff, 0xe0, 0x00, 0x10, 0x4a, 0x46, 0x49, 0x46, 0x00, 0x01, 0x01, 0x02, 0x00, 0x76];
+        let mut recoded = orig.clone();
+        encode_vec(&mut recoded);
+        decode_vec(&mut recoded);
+        assert_eq!(orig, recoded);
+    }
+
+    #[test]
+    fn synchronization_large() {
+        let mut orig = Vec::new();
+        for i in 0..1_000_000 {
+            orig.push(0xff);
+            orig.push(i as u8);
+        }
+
+        let mut recoded = orig.clone();
+        encode_vec(&mut recoded);
+        decode_vec(&mut recoded);
+        assert_eq!(orig, recoded);
     }
 }
